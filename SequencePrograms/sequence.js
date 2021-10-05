9ae4d5f037157d312da65926c5fa5a8eebea33d3
Arpitha Bhat <arpithabht12@gmail.com>
2021-10-05 19:32:11 +0530

    ADDED: generate two dice numbers and print their sum

--- SequencePrograms/sequence.js
+++ SequencePrograms/sequence.js
@@ -3,3 +3,13 @@ console.log(singleDigit);
 
 let diceNumber = 1 + Math.floor(Math.random() * 6);
 console.log(diceNumber);
+
+function getDiceNumber(){
+    return 1 + Math.floor(Math.random() * 6);
+}
+
+let dice1 = getDiceNumber();
+let dice2 = getDiceNumber();
+let add = dice1+dice2;
+console.log("Dice Numbers: "+dice1,dice2);
+console.log("Dice Sum: "+add);
