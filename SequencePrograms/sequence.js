b9bb780fd89c09c42118536ab0555528a1ca9aaa
Arpitha Bhat <arpithabht12@gmail.com>
2021-10-05 19:33:20 +0530

    ADDED: generate two digit random number and print their sum and average

--- SequencePrograms/sequence.js
+++ SequencePrograms/sequence.js
@@ -13,3 +13,17 @@ let dice2 = getDiceNumber();
 let add = dice1+dice2;
 console.log("Dice Numbers: "+dice1,dice2);
 console.log("Dice Sum: "+add);
+
+function getTwoDigitRandomNumber(min,max){
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+let sum = 0;
+for(let i=1;i<=5;i++) {
+    let number = getTwoDigitRandomNumber(10,99);
+    console.log("Number: "+number);
+    sum += number;
+}
+console.log("Sum: "+sum);
+let average = sum/5;
+console.log("Average: "+average);
