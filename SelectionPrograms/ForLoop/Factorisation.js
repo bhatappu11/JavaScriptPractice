23675744d6b037d30f970c41c899c2ca3eeba5c0
Arpitha Bhat <arpithabht12@gmail.com>
2021-10-06 17:01:04 +0530

    REFACTORED: stored the factors into array and printed it

--- SelectionPrograms/ForLoop/Factorisation.js
+++ SelectionPrograms/ForLoop/Factorisation.js
@@ -4,14 +4,15 @@ const readline = require('readline').createInterface({
     output: process.stdout
 });
 readline.question('Enter the value of n: ', number => {
-    for(let i = 2; i*i<= number; i++) {
-        while(number%i == 0) {
-           console.log(i+" ");
-           number = number/i;
-        }
-     }
-     if(number >2) {
-        console.log(number);
-     }
-    
+   let factorArr = [];
+   for(let i = 2; i*i<= number; i++) {
+       while(number%i == 0) {
+          factorArr.push(i);
+          number = number/i;
+       }
+    }
+    if(number >2) {
+       factorArr.push(i);
+    }
+   console.log("Prime Factors: "+factorArr);
 });
