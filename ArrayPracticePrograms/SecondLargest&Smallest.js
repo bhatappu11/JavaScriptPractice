560fab31283dec94b8fc6191d57c4cf6d5e94d5b
Arpitha Bhat <arpithabht12@gmail.com>
2021-10-06 16:48:16 +0530

    ADDED: find second largest and smallest in the array without using sort

--- ArrayPracticePrograms/SecondLargest&Smallest.js
+++ ArrayPracticePrograms/SecondLargest&Smallest.js
@@ -7,3 +7,27 @@ for(let index=0;index<10;index++){
     randomArr.push(getThreeDigitRandomNumber(100,999));
 }
 console.log("Array: "+randomArr);
+let biggest = randomArr[0];
+let secondBiggest = randomArr[0];
+    for(let i=0;i<randomArr.length;i++){
+        if(randomArr[i]>biggest){
+            secondBiggest = biggest;
+            biggest = randomArr[i];
+        }
+        else if(randomArr[i]>secondBiggest && randomArr[i]!=biggest)
+            secondBiggest = randomArr[i];
+    }
+    
+console.log("Second largest: "+secondBiggest);
+let smallest = randomArr[0];
+let secondSmallest = randomArr[0];
+    for(let i=0;i<randomArr.length;i++){
+        if(randomArr[i]<smallest){
+            secondSmallest = smallest;
+            smallest = randomArr[i];
+        }
+        else if(randomArr[i]<secondSmallest && randomArr[i]!=smallest)
+            secondSmallest = randomArr[i];
+    }
+    
+console.log("Second smallest: "+secondSmallest);
